ea428595a9eb26d0d8d71a6019c9f308b392cbf3
Muah <ctf0@users.noreply.github.com>
2017-09-12 04:53:32 +0200

    Update styles.blade.php

--- styles.blade.php
+++ styles.blade.php
@@ -1,22 +1,22 @@
 <style type="text/css">
     @import url('https://fonts.googleapis.com/css?family=Droid+Sans');
 
-html {
-    font-size: 14px;
-    font-family: 'Droid Sans', sans-serif;
-    color: #222;
-}
-body {
-    margin: 0;
-}
-hr {
-    border: 0;
-    margin: 0;
-    height: 1px;
-    background-color: #ccc;
-}
-.wrapper {
-    width: 50%;
-    margin: 8em auto 0;
-}
+    html {
+        font-size: 14px;
+        font-family: 'Droid Sans', sans-serif;
+        color: #222;
+    }
+    body {
+        margin: 0;
+    }
+    hr {
+        border: 0;
+        margin: 0;
+        height: 1px;
+        background-color: #ccc;
+    }
+    .wrapper {
+        width: 50%;
+        margin: 8em auto 0;
+    }
 </style>
