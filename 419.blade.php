edcc30fd49934f9b63f38d0c913f09e15e43f663
Muah <muah003@gmail.com>
2017-09-12 22:11:03 +0200

    add 419

---
+++ 419.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Error 419 - Authentication Timeout</title>
+    <meta name="viewport" content="width=device-width">
+    @include('errors.styles')
+</head>
+<body>
+    <div class="wrapper">
+        <h1>Page Expired</h1>
+        <h2>Server Error: 419 (Authentication Timeout)</h2>
+        <hr>
+        <h3>What does this mean?</h3>
+        <p>
+            Previously Valid Authentication Has Expired, Please refresh and try again
+        </p>
+    </div>
+</body>
+</html>
